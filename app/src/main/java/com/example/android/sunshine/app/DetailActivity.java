7de3c555a8fbe71cf136e7cd163aa267a439b7c1
Samee <sameezsiddiqui@gmail.com>
2016-06-16 18:06:16 -0400

    ui and backend

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -16,15 +16,25 @@ package com.example.android.sunshine.app;
  */
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.android.sunshine.app.data.WeatherContract;
 
 public class DetailActivity extends ActionBarActivity {
 
@@ -66,23 +76,156 @@ public class DetailActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class DetailFragment extends Fragment {
+    public static class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
+
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        private static final int DETAIL_LOADER = 0;
+        private ShareActionProvider provider;
+
+        private ImageView mIconView;
+        private TextView mDateView;
+        private TextView mDescriptionView;
+        private TextView mFriendlyDateView;
+        private TextView mHighTempView;
+        private TextView mLowTempView;
+        private TextView mHumidityView;
+        private TextView mWindView;
+        private TextView mPressureView;
+
+
+        private String mForecast;
+        private static final String HASHTAG = " #SunshineApp";
+
+
+        private static final String[] FORECAST_COLUMNS = {
+                // In this case the id needs to be fully qualified with a table name, since
+                // the content provider joins the location & weather tables in the background
+                // (both have an _id column)
+                // On the one hand, that's annoying.  On the other, you can search the weather table
+                // using the location set by the user, which is only in the Location table.
+                // So the convenience is worth it.
+                WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+                WeatherContract.WeatherEntry.COLUMN_DATE,
+                WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+                WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+                WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+                WeatherContract.WeatherEntry.COLUMN_HUMIDITY,
+                WeatherContract.WeatherEntry.COLUMN_WIND_SPEED,
+                WeatherContract.WeatherEntry.COLUMN_DEGREES,
+                WeatherContract.WeatherEntry.COLUMN_PRESSURE,
+                WeatherContract.WeatherEntry.COLUMN_WEATHER_ID
+        };
+
+        // These indices are tied to FORECAST_COLUMNS.  If FORECAST_COLUMNS changes, these
+        // must change.
+        static final int COL_WEATHER_ID = 0;
+        static final int COL_WEATHER_DATE = 1;
+        static final int COL_WEATHER_DESC = 2;
+        static final int COL_WEATHER_MAX_TEMP = 3;
+        static final int COL_WEATHER_MIN_TEMP = 4;
+        static final int COL_HUMIDITY = 5;
+        static final int COL_WIND= 6;
+        static final int COL_DEGREES= 7;
+        static final int COL_PRESSURE= 8;
+        static final int WEATHER_COND_ID= 9;
+
 
         public DetailFragment() {
+            setHasOptionsMenu(true);
+
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            Intent intent = getActivity().getIntent();
 
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
-            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String weather = intent.getStringExtra(Intent.EXTRA_TEXT);
-                ((TextView) rootView.findViewById(R.id.detail_text))
-                        .setText(weather);            }
+
+            mIconView = (ImageView) rootView.findViewById(R.id.image_icon);
+            mDateView = (TextView) rootView.findViewById(R.id.date);
+            mDescriptionView = (TextView) rootView.findViewById(R.id.description);
+            mFriendlyDateView = (TextView) rootView.findViewById(R.id.day_of_the_week);
+            mHighTempView = (TextView) rootView.findViewById(R.id.high_temp);
+            mLowTempView = (TextView) rootView.findViewById(R.id.low_temp);
+            mWindView = (TextView) rootView.findViewById(R.id.wind);
+            mHumidityView = (TextView) rootView.findViewById(R.id.humidity);
+            mPressureView = (TextView) rootView.findViewById(R.id.pressure);
 
             return rootView;
+        }
+
+        private Intent createShareIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mForecast + HASHTAG);
+            return shareIntent;
+
+        }
+
+        public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+            menuInflater.inflate(R.menu.fragmentdetail, menu);
+
+            MenuItem share = menu.findItem(R.id.action_share);
+            provider = (ShareActionProvider) MenuItemCompat.getActionProvider(share);
+
+            if (mForecast !=  null) {
+                provider.setShareIntent(createShareIntent());
+            }
+        }
+
+        public void onActivityCreated(Bundle savedInstanceState) {
+            getLoaderManager().initLoader(DETAIL_LOADER, null, this);
+            super.onActivityCreated(savedInstanceState);
+        }
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            Intent intent = getActivity().getIntent();
+
+            return new CursorLoader(getActivity(), intent.getData(), FORECAST_COLUMNS, null, null, null);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            if (!data.moveToFirst()) {return;}
+
+            int weatherId = data.getInt(WEATHER_COND_ID);
+            //TEMPORARY PIC
+            mIconView.setImageResource(Utility.getArtResourceForWeatherCondition(weatherId));
+
+            long date = data.getLong(COL_WEATHER_DATE);
+            String friendly = Utility.getFriendlyDayString(getActivity(), date);
+            String normalDate = Utility.getFormattedMonthDay(getActivity(), date);
+            mDateView.setText(normalDate);
+            mFriendlyDateView.setText(friendly);
+
+            String description = data.getString(COL_WEATHER_DESC);
+            mDescriptionView.setText(description);
+
+            boolean isMetric = Utility.isMetric(getActivity());
+
+            int high = data.getInt(COL_WEATHER_MAX_TEMP);
+            int low= data.getInt(COL_WEATHER_MIN_TEMP);
+            mHighTempView.setText(Utility.formatTemperature(getActivity(), high, isMetric));
+            mLowTempView.setText(Utility.formatTemperature(getActivity(), low, isMetric));
+
+
+            mHumidityView.setText(getActivity().getString(R.string.format_humidity, data.getFloat(COL_HUMIDITY)));
+            mPressureView.setText(getActivity().getString(R.string.format_pressure, data.getFloat(COL_PRESSURE)));
+
+            mWindView.setText(Utility.getFormattedWind(getActivity(), data.getFloat(COL_WIND), data.getFloat(COL_DEGREES)));
+
+            mForecast = String.format("%s - %s - %s/%s", normalDate, description, high, low);
+
+
+            if (provider != null) {
+                provider.setShareIntent(createShareIntent());
+            }
+
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
         }
     }
 }
--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,13 +12,17 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private String mLocation;
+    private final String FORECASTFRAGMENT_TAG = "FFTAG";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLocation = Utility.getPreferredLocation(this);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
+                    .add(R.id.container, new ForecastFragment(), FORECASTFRAGMENT_TAG)
                     .commit();
         }
     }
@@ -25,6 +32,19 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    protected void onResume() {
+        super.onResume();
+        if (!mLocation.equals(Utility.getPreferredLocation(this))) {
+            ForecastFragment fragment = (ForecastFragment) getSupportFragmentManager().findFragmentByTag(FORECASTFRAGMENT_TAG);
+
+            if (fragment!=null) {
+                fragment.onLocationChanged();
+
+            }
+            mLocation = Utility.getPreferredLocation(this);
+        }
     }
 
     @Override
@@ -38,6 +58,18 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             Intent settings = new Intent(this, SettingsActivity.class);
             startActivity(settings);
+        }  else if (id == R.id.show_map) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+            String location = settings.getString(getString(R.string.pref_location_key), getString(R.string.pref_default_location));
+
+            Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                    .appendQueryParameter("q", location)
+                    .build();
+            intent.setData(geoLocation);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
         }
 
         return super.onOptionsItemSelected(item);
